a6ce2c2c3d94b8266f6e0e7a0b7a7e1bd0fa79af
Martin Graf <martin.graf@student.hpi.de>
2020-07-03 15:48:24 +0200

    Add versions to packages

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
     author='Hasso Plattner Institute Chair of Digital Health - Machine Learning',
     packages=find_packages(),
     python_requires='>=3.7',
-    install_requires=['torch>=1.5', "numpy", "pandas", "pillow",
-                      "scipy", "torchvision", "tqdm", "scikit-image",
-                      "matplotlib", "kornia==0.3.*", "moviepy",
-                      "ipython", "pytest"],
+    install_requires=['torch>=1.5', "numpy>=1.18.1", "pandas>=1.0.1", "pillow>=7.0.0",
+                      "scipy>=1.4.1", "torchvision>=0.6.0", "tqdm>=4.42.1",
+                      "scikit-image>=0.16.2", "matplotlib>=3.1.3", "kornia==0.3.*",
+                      "moviepy>=1.0.1", "ipython>=7.12.0", "pytest>=5.3.5"],
 )
